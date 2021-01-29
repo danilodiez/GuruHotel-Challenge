4258aab6c61bb8dd888cae6315064d024329d156
danilodiez <danilodiez1@hotmail.com>
2021-01-29 20:17:39 -0300

    Primera definicion de paleta y header

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import './styles.css'
+
+
+
+const Header = () => {
+    return ( 
+        <div className="title-container">
+        <h1>
+        🛒 Store Finder
+        </h1>
+        </div>
+     );
+}
+ 
+
+
+
+
+export default Header;
